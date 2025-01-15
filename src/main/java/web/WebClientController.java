4ef9ae9e05876e810adf73ac0230698f8c49c326
Idoia Bergera <idoia.bergera@opendeusto.es>
2025-01-15 17:57:04 +0100

    Templates

--- src/main/java/web/WebClientController.java
+++ src/main/java/web/WebClientController.java
@@ -1,5 +1,22 @@
 package web;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import jakarta.servlet.http.HttpServletRequest;
+import proxies.RestTemplateServiceProxy;
+
 public class WebClientController {
 
+	private RestTemplateServiceProxy restTemplateServiceProxy;
+	private String token;
+	
+	 //Anade current URL y token a todas las vistas
+    @ModelAttribute
+    public void addAttributes(Model model, HttpServletRequest request) {
+        String currentUrl = ServletUriComponentsBuilder.fromRequestUri(request).toUriString();
+        model.addAttribute("currentUrl", currentUrl); 
+        model.addAttribute("token", token);
+    }
 }
